67d10db4481bdcae506841ea9ac29de38053dcbe
alexandrebuisson <38990731+alexandrebuisson@users.noreply.github.com>
2018-11-20 11:47:45 +0100

    Update conf.js

--- routes/conf.js
+++ routes/conf.js
@@ -2,7 +2,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
 user :  'root', // le nom d'utilisateur
-password :  '?1LAsdgrecrute', // le mot de passe
+password :  'petit malin... perdu mon mot de passe ne se trouve pas ici', // le mot de passe
 database :  'comparatif_games', // le nom de la base de données
 });
 module.exports = connection;
